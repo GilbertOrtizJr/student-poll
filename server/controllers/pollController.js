fe3e53ce8d3dba5d5964a04b766cea2287835692
Gilbert Ortiz Jr <gilbert.ortiz@bronxsoftware.org>
2023-05-11 14:10:46 -0400

    debug 2

--- server/controllers/pollController.js
+++ server/controllers/pollController.js
@@ -1,7 +1,7 @@
 const dbUserConn = require("../config/dbConnection"); // load the database connection
-const mysql = require("mysql"); // load the MySQL module
+console.log("loading BCRYPT")
 const bcrypt = require("bcrypt"); // load the bcrypt cypher module
-
+console.log('load BCRYPT complete')
 /***********
  * The joblist function returns the list of job titles with additional statistical information
  **********/
